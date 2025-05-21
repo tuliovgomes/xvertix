dcbea513a11c839c2613d91e5ebe9dc3c22bbfec
Pedro Reis <itspdro@outlook.com>
2025-05-21 09:57:28 -0300

    fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             @foreach (\App\Models\Post::all() as $post)
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-10">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-2">
                     <div class="text-gray-900 dark:text-gray-100">
                         <div class="flex justify-between p-6 pb-0">
                             <div class="flex flex-row items-center gap-4 pb-5">
@@ -25,15 +25,15 @@
                                 </p>
                             </div>
                         </div>
-                        <p class="w-4/5 text-start text-wrap px-6">
+                        <p class="w-11/12 text-start text-wrap px-6">
                             {{ Str::limit($post->subject, 300) }}
                         </p>
                         <div class="flex flex-row items-center gap-4 mt-4 border-t border-gray-200 p-4">
                             <a href="" class="text-gray-600 dark:text-gray-400">
-                                ({{ \Illuminate\Support\Number::forHumans($post->likes_count) }} likes)
+                                ({{ \Illuminate\Support\Number::forHumans($post->likes_count) }} curtidas)
                             </a>
                             <a href="" class="text-gray-600 dark:text-gray-400">
-                                ({{ \App\Models\Comment::where('post_id', $post->id)->count() }} comentários)
+                                ({{ \Illuminate\Support\Number::forHumans(\App\Models\Comment::where('post_id', $post->id)->count()) }} comentários)
                             </a>
                         </div>
 
